0cc90450ed30c04a08341895aa3419fb2d3ed086
DevilXfiero <deep.ch107@gmail.com>
2024-03-03 00:58:23 +0530

    Implemented infinite queries

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,29 +1,29 @@
 import axios, { AxiosRequestConfig } from "axios";
 
-
 export interface FetchResponse<T> {
-    count: number;
-    results: T[];
+  count: number;
+  next: string | null;
+  results: T[];
 }
 
 const axiosInstance = axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: 'a4427bb617b241409a4630f969fdaa3d'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "a4427bb617b241409a4630f969fdaa3d",
+  },
+});
 
-class APIClient<T>{
-    endpoint: string;
-    constructor (endpoint: string) {
-        this.endpoint = endpoint;
-    }
+class APIClient<T> {
+  endpoint: string;
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
 
-    getAll = (config: AxiosRequestConfig) => {
-        return axiosInstance.get<FetchResponse<T>>(this.endpoint,config)
-        .then(res => res.data);
-    }
-
+  getAll = (config: AxiosRequestConfig) => {
+    return axiosInstance
+      .get<FetchResponse<T>>(this.endpoint, config)
+      .then((res) => res.data);
+  };
 }
 
 export default APIClient;
